f67987340ffa663b31e038f437b3cfaad85f56ba
Aman <kumar.aman@iitg.ac.in>
2024-05-22 23:39:15 +0530

    Removed the UI overflow in Share free food page

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     Timer(const Duration(seconds: 2), () {
       Navigator.of(context).pushReplacement(MaterialPageRoute(
-        builder: (context) => const HomePage(name: 'aman', role: 'student',),
+        builder: (context) => const HomeScreen(),
       ));
     });
   }
